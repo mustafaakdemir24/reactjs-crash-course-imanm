cd85ecb72f5330ae9355dbd0852fc08a81601384
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-08-25 09:17:50 +0300

    Simple todos app

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,9 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Hello World!</h1>
-      <h2>ASAP Frontend is the best</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero hic nisi
-        necessitatibus incidunt consectetur veritatis perferendis placeat
-        voluptas minima expedita accusantium quia, maxime impedit totam
-        voluptatibus! Laboriosam modi eum obcaecati.
-      </p>
+      <h1>My Todos</h1>
     </>
   );
 }
